12aabdb3464b2df7c9a306b05a0577ee46902625
mrart <mr.artboy@gmail.com>
2026-01-09 14:27:26 +0800

    Fix service startup failure by changing gevent worker to sync worker

--- install.sh
+++ install.sh
@@ -141,7 +141,7 @@ import multiprocessing
 
 bind = "0.0.0.0:5000"
 workers = multiprocessing.cpu_count() * 2 + 1
-worker_class = "gevent"
+worker_class = "sync"
 timeout = 30
 accesslog = "/var/log/beslove/access.log"
 errorlog = "/var/log/beslove/error.log"
